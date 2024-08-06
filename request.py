34d242e8eb01fa648572e990677c39ffa10427bf
TaueIkumi <134660206+TaueIkumi@users.noreply.github.com>
2024-08-06 11:47:33 +0900

    Update request.py

--- request.py
+++ request.py
@@ -4,8 +4,8 @@ url = "http://127.0.0.1:8000/lpips/"  # FastAPIサーバーのURL
 
 # 送信する画像ファイルのパス
 game_master_path = "img/daigaku4.jpg"
-player1_path = "img/daigaku5.jpg"
-player2_path = "img/daigaku6.jpg"
+player1_path = "img/castle1.jpg"
+player2_path = "img/castle2.jpg"
 
 # 画像ファイルを開いてmultipart/form-dataで送信
 with open(game_master_path, "rb") as game_master, open(player1_path, "rb") as player1, open(player2_path, "rb") as player2:
